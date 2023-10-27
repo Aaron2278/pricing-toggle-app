0addb261c015ad023c4a04b1b05570c1b9c91e1f
Student03 <aaronsantiago542@gmail.com>
2023-10-27 18:29:58 -0400

    Pricing toggle app project phase 2 complete/no link

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Button from './components/Button';
 
 function App() {
+  const [toggle,setToggle] = useState(false)
+
+  const toggleText = toggle ? "Monthly Plans" : "Annual Plans";
+
+  const handleToggle = () => {
+    setToggle(!toggle)
+  }
+
   return (
     <div className="App">
-      <br/><br/><br/><br/>
       <h1>Flexible Pricing!</h1>
       <h2>We have a variety of options available for you...</h2>
       <h2>Choose your own price plan!</h2>
-    
-    <div className='box-space'> 
-     <div className='box'>
-      test
-     </div>
-      <div className='box'>
-
+      <br/><br/><br/><br/>
+      <div className = 'button-space'>
+      <Button toggleText={toggleText} onClick={handleToggle}/>
       </div>
-     <div className='box'>
-
-     </div>
+      <div className='box-space'> 
+        <div className='box'>
+          <div className='box-content'>
+            <h1>{toggle ? "$80" : "$10"}</h1>
+            <b>{toggle ? "Save $40!" : ""}</b>
+            <p>{toggle ? "Annually" : "Per Month"}</p>
+           <h2>{toggle ? "Standard Plan" : "Standard Plan"}</h2>
+           <b>{toggle ? "Our Standard Plan includes all your basic webpage needs!" : "Our Standard Plan includes all your basic webpage needs!"}</b>
+          </div>
+        </div>
+        <div className='box'>
+          <div className='box-content'>
+            <h1>{toggle ? "$120" : "$15"}</h1>
+            <b>{toggle ? "Save $60" : ""}</b>
+            <p>{toggle ? "Annually" : "Per Month"}</p>
+            <h2>{toggle ? "Standard+ Plan" : "Standard+ Plan"}</h2>
+            <b> {toggle ? "Our Standard+ Plan will open even more avenues for your webpage creativity!" : "Our Standard+ Plan will open even more avenues for your webpage creativity!"}</b>
+          </div>
+        </div>
+        <div className='box'>
+          <div className='box-content'>
+            <h1>{toggle ? "$200" : "$25"}</h1>
+            <b>{toggle ? "Save 100$" : ""}</b>
+            <p>{toggle ? "Annually" : "Per Month"}</p>
+            <h2>{toggle ? "Premium Plan" : "Premium Plan"}</h2>
+            <b>{toggle ? "The pinnacle of webpage productivity! Our Premium Plan will excel your webpage creation and efficiency 100% guaranteed!" : "The pinnacle of webpage productivity! Our Premium Plan will excel your webpage creation and efficiency 100% guaranteed!"}</b>
+          </div>
+        </div>
+      </div>
     </div>
-   </div>
   );
 }
 
