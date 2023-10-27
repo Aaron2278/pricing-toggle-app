cb5feb934f7581d44e3964c3950402c12adad9c1
Student03 <aaronsantiago542@gmail.com>
2023-10-27 05:58:42 -0400

    First phase of pricing tool app done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-  <br/><br/><br/><br/>
-     <h1>Flexible Pricing!</h1>
+      <br/><br/><br/><br/>
+      <h1>Flexible Pricing!</h1>
+      <h2>We have a variety of options available for you...</h2>
+      <h2>Choose your own price plan!</h2>
+    
+    <div className='box-space'> 
+     <div className='box'>
+      test
+     </div>
+      <div className='box'>
+
+      </div>
+     <div className='box'>
+
+     </div>
     </div>
+   </div>
   );
 }
 
